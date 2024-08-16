b202b018f7ce37101fcbc1082f86fde8dff35836
shota-nagato <shota.nagato@gmail.com>
2024-08-16 13:54:43 +0900

    vpc_cidrを変数に

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -2,5 +2,5 @@ module "vpc" {
   source = "../../modules/vpc"
   name = var.name
   environment =  var.environment
-  vpc_cidr = "10.0.0.0/16"
+  vpc_cidr = var.vpc_cidr
 }
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -5,3 +5,7 @@ variable "name" {
 variable "environment" {
   default = "dev"
 }
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
